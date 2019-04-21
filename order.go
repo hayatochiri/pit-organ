294dc5185dfd9919ab5338653898f2f4f521df29
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-22 02:24:42 +0900

    注文レシーバを追加

--- order.go
+++ order.go
@@ -1,5 +1,19 @@
 package pitOrgan
 
+// Receivers
+
+type ReceiverOrders struct {
+	AccountID  string
+	Connection *Connection
+}
+
+func (r *ReceiverAccountID) Orders() *ReceiverOrders {
+	return &ReceiverOrders{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
+
 // TODO: POST /v3/accounts/{accountID}/orders
 
 // TODO: GET /v3/accounts/{accountID}/orders
