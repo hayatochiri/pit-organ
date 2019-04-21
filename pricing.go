1e835b4b3351d96f70453d9cd27d713debd95e32
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-22 02:53:58 +0900

    値段取得ストリーム機能の実装

--- pricing.go
+++ pricing.go
@@ -1,7 +1,10 @@
 package pitOrgan
 
 import (
+	"bufio"
+	"encoding/json"
 	"golang.org/x/xerrors"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -27,12 +30,26 @@ type GetPricingParams struct {
 	IncludeHomeConversions JsonBool
 }
 
+type GetPricingStreamParams struct {
+	BufferSize  int
+	Instruments []string
+}
+
 // Schemas
 
 type GetPricingSchema struct {
 	Prices          []PriceDefinition           `json:"prices"`
 	HomeConversions []HomeConversionsDefinition `json:"homeConversions"`
 	Time            DateTimeDefinition          `json:"time"`
+}
+
+// Types
+
+type PriceChannels struct {
+	Price <-chan *PriceDefinition
+	Error <-chan error
+	close chan<- interface{}
+	resp  *http.Response // for test
 }
 
 func (r *ReceiverAccountID) Pricing() *ReceiverPricing {
@@ -92,5 +109,71 @@ func (r *ReceiverPricing) Stream() *ReceiverPricingStream {
 	}
 }
 
-// TODO: GET /v3/accounts/{accountID}/pricing/stream
+// GET /v3/accounts/{accountID}/pricing/stream
+func (r *ReceiverPricingStream) Get(params *GetPricingStreamParams) (*PriceChannels, error) {
+	resp, err := r.Connection.stream(
+		&requestParams{
+			method:   "GET",
+			endPoint: "/v3/accounts/" + r.AccountID + "/pricing/stream",
+			headers: []header{
+				{key: "Accept-Datetime-Format", value: "RFC3339"},
+			},
+			queries: []query{
+				{key: "instruments", value: strings.Join(params.Instruments, ",")},
+			},
+		},
+	)
+	if err != nil {
+		return nil, xerrors.Errorf("Get pricing stream canceled: %w", err)
+	}
 
+	// TODO: Catch Response code 401, 404, 405
+
+	priceCh := make(chan *PriceDefinition, params.BufferSize)
+	errorCh := make(chan error, 1)
+	closeCh := make(chan interface{})
+	go func() {
+		defer func() {
+			resp.Body.Close()
+			close(priceCh)
+		}()
+
+		reader := bufio.NewReader(resp.Body)
+
+		for {
+			select {
+			case _, ok := <-closeCh:
+				if ok == false {
+					return
+				}
+			default:
+			}
+
+			line, err := reader.ReadBytes('\n')
+			if err != nil {
+				errorCh <- xerrors.Errorf("TODO Error Message: %w", err)
+				return
+			}
+
+			data := new(PriceDefinition)
+			err = json.Unmarshal(line, data)
+			if err != nil {
+				errorCh <- xerrors.Errorf("TODO Error Message: %w", err)
+				return
+			}
+
+			if data.Type == "HEARTBEAT" {
+				continue
+			}
+
+			priceCh <- data
+		}
+	}()
+
+	return &PriceChannels{
+		Price: priceCh,
+		Error: errorCh,
+		close: closeCh,
+		resp:  resp,
+	}, nil
+}
