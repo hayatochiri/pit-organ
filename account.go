02dd7cf587975b1ff8a1b4e6564d4b5982369a7f
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-21 21:39:36 +0900

    APIからアカウント情報を取得する機能を実装

--- account.go
+++ account.go
@@ -1,9 +1,19 @@
 package pitOrgan
 
+import (
+	"golang.org/x/xerrors"
+)
+
 // Receivers
 
 type ReceiverAccounts struct {
 	Connection *Connection
+}
+
+// Schemas
+
+type GetAccountsSchema struct {
+	Accounts []AccountPropertiesDefinition `json:"accounts"`
 }
 
 func (c *Connection) Accounts() *ReceiverAccounts {
@@ -12,7 +22,31 @@ func (c *Connection) Accounts() *ReceiverAccounts {
 	}
 }
 
-// TODO: GET /v3/accounts
+// GET /v3/accounts
+func (r *ReceiverAccounts) Get() (*GetAccountsSchema, error) {
+	resp, err := r.Connection.request(
+		&requestParams{
+			method:   "GET",
+			endPoint: "/v3/accounts",
+		},
+	)
+	if err != nil {
+		return nil, xerrors.Errorf("Get accounts canceled: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var data interface{}
+	switch resp.StatusCode {
+	case 200:
+		data = new(GetAccountsSchema)
+	}
+
+	data, err = parseResponse(resp, data)
+	if err != nil {
+		return nil, xerrors.Errorf("Get accounts failed: %w", err)
+	}
+	return data.(*GetAccountsSchema), nil
+}
 
 // TODO: GET /v3/accounts/{accountID}
 
