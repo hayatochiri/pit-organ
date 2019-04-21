26199408b30a0064288b0bfcb035548988c33cb9
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-22 02:48:33 +0900

    値段取得、取得ストリームのレシーバを追加

--- pricing.go
+++ pricing.go
@@ -1,6 +1,32 @@
 package pitOrgan
 
+// Receivers
+
+type ReceiverPricing struct {
+	AccountID  string
+	Connection *Connection
+}
+
+type ReceiverPricingStream struct {
+	AccountID  string
+	Connection *Connection
+}
+
+func (r *ReceiverAccountID) Pricing() *ReceiverPricing {
+	return &ReceiverPricing{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
+
 // TODO: GET /v3/accounts/{accountID}/pricing
+
+func (r *ReceiverPricing) Stream() *ReceiverPricingStream {
+	return &ReceiverPricingStream{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
 
 // TODO: GET /v3/accounts/{accountID}/pricing/stream
 
