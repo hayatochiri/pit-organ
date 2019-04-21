3463f1b313650a9aa8f656c34b3f2dfffae07c67
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-21 20:29:38 +0900

    テスト用に新規コネクションを作成して返す関数を実装

--- pit_organ_test.go
+++ pit_organ_test.go
@@ -3,6 +3,9 @@ package pitOrgan
 import (
 	"github.com/joho/godotenv"
 	"golang.org/x/xerrors"
+	"os"
+	"testing"
+	"time"
 )
 
 func init() {
@@ -10,4 +13,15 @@ func init() {
 	if err != nil {
 		panic(xerrors.Errorf("Error loading .env file: %w", err))
 	}
+}
+
+func newConnection(t *testing.T, env OandaEnvironment) *Connection {
+
+	connection := &Connection{
+		Token:       os.Getenv("TOKEN"),
+		Environemnt: env,
+		Timeout:     time.Second * 10,
+	}
+
+	return connection
 }
