fb7606e0065f99f7bac5a1c951fa8bd6ad1474b5
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-22 02:52:52 +0900

    値段を取得する機能の実装

--- pricing.go
+++ pricing.go
@@ -1,5 +1,11 @@
 package pitOrgan
 
+import (
+	"golang.org/x/xerrors"
+	"strings"
+	"time"
+)
+
 // Receivers
 
 type ReceiverPricing struct {
@@ -12,6 +18,23 @@ type ReceiverPricingStream struct {
 	Connection *Connection
 }
 
+// Params
+
+type GetPricingParams struct {
+	Instruments            []string
+	Since                  time.Time
+	IncludeUnitsAvailable  JsonBool
+	IncludeHomeConversions JsonBool
+}
+
+// Schemas
+
+type GetPricingSchema struct {
+	Prices          []PriceDefinition           `json:"prices"`
+	HomeConversions []HomeConversionsDefinition `json:"homeConversions"`
+	Time            DateTimeDefinition          `json:"time"`
+}
+
 func (r *ReceiverAccountID) Pricing() *ReceiverPricing {
 	return &ReceiverPricing{
 		AccountID:  r.AccountID,
@@ -19,7 +42,48 @@ func (r *ReceiverAccountID) Pricing() *ReceiverPricing {
 	}
 }
 
-// TODO: GET /v3/accounts/{accountID}/pricing
+// GET /v3/accounts/{accountID}/pricing
+func (r *ReceiverPricing) Get(params *GetPricingParams) (*GetPricingSchema, error) {
+	resp, err := r.Connection.request(
+		&requestParams{
+			method:   "GET",
+			endPoint: "/v3/accounts/" + r.AccountID + "/pricing",
+			headers: []header{
+				{key: "Accept-Datetime-Format", value: "RFC3339"},
+			},
+			queries: func() []query {
+				q := make([]query, 0, 4)
+				q = append(q, query{key: "instruments", value: strings.Join(params.Instruments, ",")})
+				if b, err := params.IncludeUnitsAvailable.string(); err == nil {
+					q = append(q, query{key: "includeUnitsAvailable", value: b})
+				}
+				if b, err := params.IncludeHomeConversions.string(); err == nil {
+					q = append(q, query{key: "includeHomeConversions", value: b})
+				}
+				if !params.Since.IsZero() {
+					q = append(q, query{key: "since", value: params.Since.Format(time.RFC3339Nano)})
+				}
+				return q
+			}(),
+		},
+	)
+	if err != nil {
+		return nil, xerrors.Errorf("Get pricing canceled: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var data interface{}
+	switch resp.StatusCode {
+	case 200:
+		data = new(GetPricingSchema)
+	}
+
+	data, err = parseResponse(resp, data)
+	if err != nil {
+		return nil, xerrors.Errorf("Get pricing failed: %w", err)
+	}
+	return data.(*GetPricingSchema), nil
+}
 
 func (r *ReceiverPricing) Stream() *ReceiverPricingStream {
 	return &ReceiverPricingStream{
