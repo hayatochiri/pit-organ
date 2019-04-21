9cd71f427e84164a6abdd101ff5ee5771f18ce7f
hayatochiri <dev@sucret.sakura.ne.jp>
2019-04-21 19:37:34 +0900

    API接続先の定数と定数から接続先を取得する関数を追加

--- pit_organ.go
+++ pit_organ.go
@@ -1,10 +1,22 @@
 package pitOrgan
 
 import (
+	"net/url"
 	"time"
 )
 
 type OandaEnvironment int
+
+const (
+	OandaPractice OandaEnvironment = iota + 1
+	OandaLive
+	oandaDummy
+)
+
+type baseURLs struct {
+	rest   *url.URL
+	stream *url.URL
+}
 
 type Connection struct {
 	Token       string
@@ -12,3 +24,32 @@ type Connection struct {
 	Timeout     time.Duration
 }
 
+func oandaBaseURL(env OandaEnvironment) *baseURLs {
+	var urls *baseURLs
+
+	switch env {
+	case OandaPractice:
+		urls = &baseURLs{
+			rest:   parseURL("https://api-fxpractice.oanda.com"),
+			stream: parseURL("https://stream-fxpractice.oanda.com"),
+		}
+	case OandaLive:
+		urls = &baseURLs{
+			rest:   parseURL("https://api-fxtrade.oanda.com"),
+			stream: parseURL("https://stream-fxtrade.oanda.com"),
+		}
+	case oandaDummy:
+		urls = &baseURLs{
+			rest:   parseURL("https://192.0.2.1"),
+			stream: parseURL("https://192.0.2.2"),
+		}
+	}
+
+	return urls
+}
+
+func parseURL(urlString string) *url.URL {
+	urlStructure, _ := url.Parse(urlString)
+
+	return urlStructure
+}
