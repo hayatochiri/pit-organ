fb80c87a41cb32af801f8938a8fbc5a1e880e28d
hayatochiri <dev@sucret.sakura.ne.jp>
2019-05-04 06:20:09 +0900

    銘柄、過去の銘柄取得リクエストのレシーバを追加

--- instrument.go
+++ instrument.go
@@ -1,5 +1,41 @@
 package pitOrgan
 
+// Receivers
+
+type ReceiverInstruments struct {
+	Connection *Connection
+}
+
+type ReceiverInstrument struct {
+	Instrument string
+	Connection *Connection
+}
+
+type ReceiverInstrumentCandles struct {
+	Instrument string
+	Connection *Connection
+}
+
+func (c *Connection) Instruments() *ReceiverInstruments {
+	return &ReceiverInstruments{
+		Connection: c,
+	}
+}
+
+func (r *ReceiverInstruments) Instruments(i string) *ReceiverInstrument {
+	return &ReceiverInstrument{
+		Instrument: i,
+		Connection: r.Connection,
+	}
+}
+
+func (r *ReceiverInstrument) Candles() *ReceiverInstrumentCandles {
+	return &ReceiverInstrumentCandles{
+		Instrument: r.Instrument,
+		Connection: r.Connection,
+	}
+}
+
 // TODO: GET /v3/instruments/{instrument}/candles
 
 // TODO: GET /v3/instruments/{instrument}/orderBook
