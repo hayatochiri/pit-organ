854c3f9aa5e3b8be839bffdcfa81f52048b08b4b
hayatochiri <dev@sucret.sakura.ne.jp>
2019-05-06 07:17:02 +0900

    トランザクション取得APIのレシーバを追加

--- transaction.go
+++ transaction.go
@@ -1,5 +1,44 @@
 package pitOrgan
 
+// Receivers
+
+type ReceiverTransactions struct {
+	AccountID  string
+	Connection *Connection
+}
+
+type ReceiverTransactionsIdrange struct {
+	AccountID  string
+	Connection *Connection
+}
+
+type ReceiverTransactionsStream struct {
+	AccountID  string
+	Connection *Connection
+}
+
+
+func (r *ReceiverAccountID) Transactions() *ReceiverTransactions {
+	return &ReceiverTransactions{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
+
+func (r *ReceiverTransactions) Idrange() *ReceiverTransactionsIdrange {
+	return &ReceiverTransactionsIdrange{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
+
+func (r *ReceiverTransactions) Stream() *ReceiverTransactionsStream {
+	return &ReceiverTransactionsStream{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
+
 // TODO: GET /v3/accounts/{accountID}/transactions
 
 // TODO: GET /v3/accounts/{accountID}/transactions/{transactionID}
