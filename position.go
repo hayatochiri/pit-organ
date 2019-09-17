470c31b5b3097604b98b7819c0c7311e2031cd5d
hayatochiri <dev@sucret.sakura.ne.jp>
2019-09-17 16:58:48 +0900

    ポジションを取得する機能を追加

--- position.go
+++ position.go
@@ -1,6 +1,64 @@
 package pitOrgan
 
-// TODO: GET /v3/accounts/{accountID}/positions
+import (
+	"golang.org/x/xerrors"
+)
+
+/* Receivers */
+
+type ReceiverPositions struct {
+	AccountID  string
+	Connection *Connection
+}
+
+func (r *ReceiverAccountID) Positions() *ReceiverPositions {
+	return &ReceiverPositions{
+		AccountID:  r.AccountID,
+		Connection: r.Connection,
+	}
+}
+
+/* Params */
+
+/* Schemas */
+
+type GetPositionsSchema struct {
+	Positions         []PositionDefinition    `json:"positions,omitempty"`
+	LastTransactionID TransactionIDDefinition `json:"lastTransactionID,omitempty"`
+}
+
+/* Errors */
+
+/* API */
+
+// GET /v3/accounts/{accountID}/positions
+func (r *ReceiverPositions) Get() (*GetPositionsSchema, error) {
+	resp, err := r.Connection.request(
+		&requestParams{
+			method:   "Get",
+			endPoint: "/v3/accounts/" + r.AccountID + "/positions",
+			headers: []header{
+				{key: "Accept-Datetime-Format", value: "RFC3339"},
+			},
+		},
+	)
+	if err != nil {
+		return nil, xerrors.Errorf("Get positions canceled: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var data interface{}
+	switch resp.StatusCode {
+	case 200:
+		data = new(GetPositionsSchema)
+	}
+
+	data, err = parseResponse(resp, data, r.Connection.strict)
+	if err != nil {
+		return nil, xerrors.Errorf("Get positions failed: %w", err)
+	}
+	return data.(*GetPositionsSchema), nil
+}
 
 // TODO: GET /v3/accounts/{accountID}/openPositions
 
