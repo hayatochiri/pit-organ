7b997b8347dd0c077b3be8e5d53d923d8d628113
hayatochiri <dev@sucret.sakura.ne.jp>
2019-09-17 17:32:54 +0900

    オープンポジションを取得する機能のテストを追加

--- position_test.go
+++ position_test.go
@@ -16,3 +16,34 @@ func Test_Positions(t *testing.T) {
 
 	t.Logf("Response:\n%s", spew.Sdump(data))
 }
+
+func Test_OpenPositions(t *testing.T) {
+	connection := newConnection(t, OandaPractice)
+	accountID := Getenv("ACCOUNT_ID")
+	accountIDReceiver := connection.Accounts().AccountID(accountID)
+
+	paramsArray := []MarketOrderRequestDefinition{
+		{Type: "MARKET", Instrument: "EUR_USD", Units: "100"},
+		{Type: "MARKET", Instrument: "USD_JPY", Units: "150"},
+		{Type: "MARKET", Instrument: "EUR_JPY", Units: "200"},
+	}
+	for _, orderRequest := range paramsArray {
+		params := &PostOrdersParams{
+			Body: PostOrdersBodyParams{
+				Order: orderRequest,
+			},
+		}
+
+		_, err := accountIDReceiver.Orders().Post(params)
+		if err != nil {
+			t.Fatalf("Error occurred.\n%+v", err)
+		}
+	}
+
+	data, err := accountIDReceiver.OpenPositions().Get()
+	if err != nil {
+		t.Fatalf("Error occurred.\n%+v", err)
+	}
+
+	t.Logf("Response:\n%s", spew.Sdump(data))
+}
