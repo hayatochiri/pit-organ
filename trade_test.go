065e469bed1febc81e898f03805a2ea568ebaeef
hayatochiri <dev@sucret.sakura.ne.jp>
2019-09-21 03:27:52 +0900

    指定したトレードを取得する機能のテストを追加

--- trade_test.go
+++ trade_test.go
@@ -68,3 +68,34 @@ func Test_OpenTrades(t *testing.T) {
 
 	t.Logf("Response:\n%s", spew.Sdump(data))
 }
+
+func Test_TradeSpecifier(t *testing.T) {
+	connection := newConnection(t, OandaPractice)
+	accountID := Getenv("ACCOUNT_ID")
+	accountIDReceiver := connection.Accounts().AccountID(accountID)
+
+	order := func() *PostOrdersSchema {
+		params := &PostOrdersParams{
+			Body: PostOrdersBodyParams{
+				Order: MarketOrderRequestDefinition{
+					Type:       "MARKET",
+					Instrument: "EUR_USD",
+					Units:      "1",
+				},
+			},
+		}
+		order, err := accountIDReceiver.Orders().Post(params)
+		if err != nil {
+			t.Fatalf("Error occurred.\n%+v", err)
+		}
+
+		return order
+	}()
+
+	data, err := accountIDReceiver.Trades().TradeSpecifier(order.LastTransactionID).Get()
+	if err != nil {
+		t.Fatalf("Error occurred.\n%+v", err)
+	}
+
+	t.Logf("Response:\n%s", spew.Sdump(data))
+}
