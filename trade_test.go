32b8973d6c20b000776fa2c11f0836178c5c9a58
hayatochiri <dev@sucret.sakura.ne.jp>
2019-09-24 05:45:14 +0900

    トレードを指定したオーダーを行う機能のテストを追加

--- trade_test.go
+++ trade_test.go
@@ -2,6 +2,7 @@ package pitOrgan
 
 import (
 	"github.com/davecgh/go-spew/spew"
+	"strconv"
 	"testing"
 )
 
@@ -163,6 +164,53 @@ func Test_TradeSpecifierClientExtensions(t *testing.T) {
 	}
 
 	data, err := accountIDReceiver.Trades().TradeSpecifier(order.LastTransactionID).ClientExtensions().Put(params)
+	if err != nil {
+		t.Fatalf("Error occurred.\n%+v", err)
+	}
+
+	t.Logf("Response:\n%s", spew.Sdump(data))
+}
+
+func Test_TradeSpecifierOrders(t *testing.T) {
+	connection := newConnection(t, OandaPractice)
+	accountID := Getenv("ACCOUNT_ID")
+	accountIDReceiver := connection.Accounts().AccountID(accountID)
+
+	order := func() *PostOrdersSchema {
+		params := &PostOrdersParams{
+			Body: PostOrdersBodyParams{
+				Order: MarketOrderRequestDefinition{Type: "MARKET", Instrument: "USD_JPY", Units: "10000"},
+			},
+		}
+
+		order, err := accountIDReceiver.Orders().Post(params)
+		if err != nil {
+			t.Fatalf("Error occurred.\n%+v", err)
+		}
+
+		return order
+	}()
+
+	price, err := strconv.ParseFloat(order.OrderFillTransaction.Price, 32)
+	if err != nil {
+		t.Fatalf("Error occurred.\n%+v", err)
+	}
+
+	params := &PutTradeSpecifierOrdersParams{
+		Body: &PutTradeSpecifierOrdersBodyParams{
+			TakeProfit: &TakeProfitDetailsDefinition{
+				Price: strconv.FormatFloat(price+1.0, 'f', 4, 32),
+			},
+			StopLoss: &StopLossDetailsDefinition{
+				Price: strconv.FormatFloat(price-1.0, 'f', 4, 32),
+			},
+			TrailingStopLoss: &TrailingStopLossDetailsDefinition{
+				Distance: "0.05",
+			},
+		},
+	}
+
+	data, err := accountIDReceiver.Trades().TradeSpecifier(order.LastTransactionID).Orders().Put(params)
 	if err != nil {
 		t.Fatalf("Error occurred.\n%+v", err)
 	}
